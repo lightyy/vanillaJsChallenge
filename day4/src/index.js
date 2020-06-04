6f0790363112f2559b2647478e1f3896fc4cae4e
lightyy <lightyy22@gmail.com>
2020-06-05 00:06:11 +0900

    day4 finish

--- day4/src/index.js
+++ day4/src/index.js
@@ -1,1 +1,16 @@
+// <⚠️ DONT DELETE THIS ⚠️>
 import "./styles.css";
+// <⚠️ /DONT DELETE THIS ⚠️>
+const body = document.querySelector("body");
+function resizeHandler() {
+  const currentWidth = window.innerWidth;
+  if (currentWidth > 700 && currentWidth < 1200) {
+    body.style.backgroundColor = "#904FAD";
+  } else if (currentWidth >= 1200) {
+    body.style.backgroundColor = "#EEBC12";
+  } else {
+    body.style.backgroundColor = "#2E8CD5";
+  }
+}
+window.addEventListener("resize", resizeHandler);
+resizeHandler();
