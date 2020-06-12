bf0b965f31324128f0748676d5be4a009f298e59
lightyy <lightyy22@gmail.com>
2020-06-13 01:43:46 +0900

    day11,12 finish

--- day11/src/index.js
+++ day11/src/index.js
@@ -1,27 +1,81 @@
 // <⚠️ DONT DELETE THIS ⚠️>
 // import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
-const calculator = document.querySelector(".calculator"),
-  btn = calculator.querySelectorAll("button"),
-  output = calculator.querySelector("output");
+const output = document.querySelector(".output");
 
-function calculate(num) {
-  if (typeof parseInt(num) === "number" && parseInt(num)) {
-    output.value += parseInt(num);
-  } else {
-    if (num === "C") {
-      output.value = "";
-    } else if (num === "+") {
-      output.value;
+const OPERATOR = {
+  plus: function(a, b) {return a + b},
+  minus: function(a, b) {return a - b},
+  multiply: function(a, b) {return a * b},
+  divison: function(a, b) {return a / b},
+};
+
+let RESULT = "",
+  OP_TEXT = "",
+  FLAG = true,
+  CAN_CALC = false;
+
+function calculate(btn) {
+  const name = btn.className;
+
+  if (name == "btn num") {
+    const num = btn.textContent;
+    if (FLAG) {
+      output.textContent = num;
+      FLAG = false;
+    } else {
+      output.textContent = output.textContent + btn.textContent;
     }
+    if (output.textContent === "") FLAG = true;
+  } else if (name == "btn operator") {
+    equal();
+    if (!CAN_CALC) {
+      OP_TEXT = btn.textContent;
+      RESULT = output.textContent + OP_TEXT;
+      CAN_CALC = true;
+    }
+    FLAG = true;
+  } else if (name == "btn cleaning") {
+    RESULT = "";
+    output.textContent = RESULT;
+    FLAG = true;
+    CAN_CALC = false;
+  } else if (name == "btn equal") {
+    equal();
+  }
+}
+
+function equal() {
+  if (CAN_CALC && !FLAG) {
+    RESULT += output.textContent;
+    const numArray = RESULT.split(OP_TEXT);
+    const num1 = parseInt(numArray[0]);
+    const num2 = parseInt(numArray[1]);
+    switch (OP_TEXT) {
+      case "+":
+        RESULT = OPERATOR.plus(num1, num2);
+        break;
+      case "-":
+        RESULT = OPERATOR.minus(num1, num2);
+        break;
+      case "*":
+        RESULT = OPERATOR.multiply(num1, num2);
+        break;
+      case "/":
+        RESULT = OPERATOR.divison(num1, num2);
+        break;
+    }
+    FLAG = true;
+    CAN_CALC = false;
+    output.textContent = RESULT;
   }
 }
 
 function clickBtnHandler(e) {
   e.preventDefault();
-  //   console.dir(output);
-  if (e.target.tagName === "BUTTON") {
-    calculate(e.target.textContent);
+  const target = e.target;
+  if (target.tagName === "BUTTON") {
+    calculate(target);
   }
 }
 
